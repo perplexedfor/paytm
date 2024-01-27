a4c6e93d0755790e90ba0e283e3aa61b20b38723
perplexedfor <gusainabhinav037@gmail.com>
2024-01-27 10:34:20 +0530

    backend Done!!

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,17 @@
 
 const express = require('express');
 const userRouter = require("./user");
+const accountRouter = require("./account")
+
 // const app = express();
 // const PORT = 3000;
  
 // Single routing
 const router = express.Router();
  
-router.use("/user",userRouter)
+router.use("/user",userRouter);
+router.use("/account",accountRouter);
+
 
 module.exports = router;
  
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,8 @@
 import { z } from "zod";
-const { User } = require("../db");
+const { User, Account } = require("../db");
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require("../config");
+const { authMiddleware } = require('../middleware');
 
 const express = require('express');
 const router = express.Router();
@@ -16,6 +17,51 @@ const signinBody = z.object({
     username: z.string().email(),
     password: z.string()
 })
+const updateBody = z.object({
+    password: Zod.string().optional(),
+    firstName: z.string().optional(),
+    lastName: z.string().optional(),
+})
+
+router.put('/',authMiddleware,async (req,res) =>{
+    const { success } = updateBody.safeParse(req.body)
+    if(!success){
+        res.status(411).json({
+            message: "Error while updating information"
+        })
+    }
+    await User.updateOne(req.body, {
+        _id: req.userId
+    })
+    res.json({
+        message:"Updated Successfully"
+    })
+})
+
+router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
 
 router.post("/signup",async (req,res) => {
     const success = SignupBody.safeParse(req.body);
@@ -24,6 +70,7 @@ router.post("/signup",async (req,res) => {
             message: "Email already taken/Incorrect inputs"
         })
     }
+
     const existingUser = await User.findOne({
         username: req.body.username
     })
@@ -38,7 +85,13 @@ router.post("/signup",async (req,res) => {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
     })
+    
     const userId = user._id;
+    const balance = await Account.create({
+        user: userId,
+        balance: 1 + Math.random()*10000
+    })
+
     const token = jwt.sign({
         userId
     },JWT_SECRET);
